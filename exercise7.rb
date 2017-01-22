ad304b874ae329fcdcbd7a0255f39f1d5cede8d8
Justin <justintenser@me.com>
2017-01-22 12:26:52 -0500

    finished

--- exercise7.rb
+++ exercise7.rb
@@ -6,7 +6,7 @@ students = {
   :cohort3 => 22
 }
 
-# Create a method that displays the name and number of students for each cohort, like so:
+# Create a method that displays the name and number of students for each cohort:
 
   def display(h)
 h.each do |a, b|
@@ -27,13 +27,20 @@ p "output all of the cohort names: "
 p students.keys
 
 
-# 5. The classrooms have been expanded! Increase each cohort size by 5% and display the new results.
+# 5. Increase each cohort size by 5% and display the new results.
 p "Increase cohort size by 5%: "
-students.each  do |k, v|
-  students[k] = v*1.05
+students.each  do |a, b|
+  students[a] = b*1.05
 end
 display(students)
 
 # 6. "Delete the 2nd cohort: "
 students.delete(:cohort2)
 display(students
+
+# 7. BONUS: Calculate the total number of students across all cohorts using each and a variable to keep track of the total. Output the result.
+total = 0
+students.each do |a, b|
+  total += b
+end
+p "total number of students are #{total}"
